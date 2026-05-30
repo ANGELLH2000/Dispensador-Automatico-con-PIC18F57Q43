e2ef39e676ec35bb8eb563ccc1b5f19b81947d41
Angel Leon H. <angeleonh2000@gmail.com>
2026-05-29 21:10:15 -0500

    Librería del motor paso a paso

--- TF-Micro.X/maincode.c
+++ TF-Micro.X/maincode.c
@@ -1,6 +1,9 @@
 #include <xc.h>
 #include "cabecera.h"
-#define _XTAL_FREQ 48000000UL  //unsigned long Se le especifca al preograma para que sepa a cuanto trabaja
+#include "motor_paso.h"
+
+Stepper motor1;
+Stepper motor2;
 
 void config(void) {
     //conf mod de oscilador
@@ -12,6 +15,58 @@ void config(void) {
 }
 void main(void) {
     config();
-    
-    return;
+   /*
+     * Inicialización del motor 1.
+     *
+     * Motor 1 conectado al puerto D, parte baja:
+     * RD0, RD1, RD2 y RD3.
+     */
+    Stepper_Init(&motor1, &LATD, &TRISD, &ANSELD, STEPPER_LOW_NIBBLE);
+
+    /*
+     * Inicialización del motor 2.
+     *
+     * Motor 2 conectado al puerto D, parte alta:
+     * RD4, RD5, RD6 y RD7.
+     */
+    Stepper_Init(&motor2, &LATD, &TRISD, &ANSELD, STEPPER_HIGH_NIBBLE);
+
+    while(1)
+    {
+        /*
+         * Ejecuta una vuelta completa del motor 1 en sentido horario.
+         */
+        Stepper_fullTurn_CW(&motor1);
+
+        __delay_ms(1000);
+
+        /*
+         * Ejecuta una vuelta completa del motor 2 en sentido horario.
+         */
+        Stepper_fullTurn_CW(&motor2);
+
+        __delay_ms(1000);
+
+        /*
+         * Ejecuta una vuelta completa del motor 1 en sentido antihorario.
+         */
+        Stepper_fullTurn_CCW(&motor1);
+
+        __delay_ms(1000);
+
+        /*
+         * Ejecuta una vuelta completa del motor 2 en sentido antihorario.
+         */
+        Stepper_fullTurn_CCW(&motor2);
+
+        __delay_ms(1000);
+
+        /*
+         * Desactiva ambos motores.
+         */
+        Stepper_Off(&motor1);
+        Stepper_Off(&motor2);
+
+        __delay_ms(1000);
+    }
 }
